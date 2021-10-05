ee0a560c0a8924d584ad57920e832a854f56a5fd
dnigh <dnigh@users.noreply.github.com>
2021-10-05 12:43:05 -0700

    Update sha512_mb.c
    test static inline for sha512_pad

--- arch/x86/crypto/sha512-mb/sha512_mb.c
+++ arch/x86/crypto/sha512-mb/sha512_mb.c
@@ -116,7 +116,7 @@ inline void sha512_init_digest(uint64_t *digest)
 	memcpy(digest, initial_digest, sizeof(initial_digest));
 }
 
-inline uint32_t sha512_pad(uint8_t padblock[SHA512_BLOCK_SIZE * 2],
+static inline uint32_t sha512_pad(uint8_t padblock[SHA512_BLOCK_SIZE * 2],
 			 uint32_t total_len)
 {
 	uint32_t i = total_len & (SHA512_BLOCK_SIZE - 1);
